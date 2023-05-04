dadde334bb80006b5716003bb8d4027bdb58d88c
ee panyakorn <panyakorn40@gmail.com>
2023-05-04 15:49:57 +0700

    🔥 chore(posts.go): comment out unused imports, variables, and function calls to clean up code and improve readability

--- go-zero-demo/posts/posts.go
+++ go-zero-demo/posts/posts.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
-
-	"github.com/tal-tech/go-zero/core/conf"
-	"github.com/tal-tech/go-zero/example/graceful/dns/api/handler"
-	"github.com/tal-tech/go-zero/rest"
-	"honnef.co/go/tools/config"
 )
 
 var configFile = flag.String("f", "etc/posts-api.yaml", "the config file")
 
 func main() {
-	flag.Parse()
+	// flag.Parse()
 
-	var c config.Config
-	conf.MustLoad(*configFile, &c)
+	// var c config.Config
+	// conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf)
-	defer server.Stop()
+	// server := rest.MustNewServer(c.RestConf)
+	// defer server.Stop()
 
-	ctx := svc.NewServiceContext(c)
-	handler.RegisterHandlers(server, ctx)
+	// ctx := svc.NewServiceContext(c)
+	// handler.RegisterHandlers(server, ctx)
 
-	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
-	server.Start()
+	// fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	// server.Start()
 }
